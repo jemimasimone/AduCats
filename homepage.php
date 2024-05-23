28706cfa1f86afe4e0d18e9d97d3291ca8141c63
Nikol <nicolecolein29@gmail.com>
2024-05-23 19:53:11 +0800

    New NavBAr:Homepage

--- homepage.php
+++ homepage.php
@@ -6,33 +6,38 @@ check_user_role(1);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AduCats</title>
-    
+
 
     <!-- LINKS -->
     <link rel="stylesheet" href="style/homepage.css">
     <link rel="icon" href="img/WhiteLogoCat.png">
 
 </head>
+
 <body>
- 
+
     <!--NAVBAR-->
 
     <nav class="nav_bar">
 
         <div class="left-navbar">
-            <a href="adoption.php">Adoption Request</a> 
-            <a href="donation.php">Donate</a> 
+            <a id="profilelnk" href=" ">Profile</a>
+            <a id="adoptionlnk" href="adoption.php">Adoption Request</a>
+            <a id="donatelnk" href="donation.php">Donate</a>
+
         </div>
-        
-        <img id="AduCatsLogo" src ="img/AduCatsLogo.png">
-        
+
+        <img id="AduCatsLogo" src="img/AduCatsLogo.png">
+
         <div class="right-navbar">
-            <a href="cat-profile.php">Cat Profiles</a> 
-            <a href="php/logout.php"><button>Logout</button></a>
+            <a id="recordslnk" href=" ">Records</a>
+            <a id="CatProflnk" href="cat-profile.php">Cat Profiles</a>
+            <a id="Logoutlnk" href="php/logout.php"><button>Logout</button></a>
             <?php
             // if ($_SESSION['user_role'] == 2) {
             //     echo '<p><a href="adminSide.php">Go to Admin side</a>.</p>';
@@ -41,97 +46,98 @@ check_user_role(1);
             // }
             ?>
         </div>
-       
-      </nav>
+
+    </nav>
 
 
-       <!--Etoh buong content ng Homepage-->
+    <!--Etoh buong content ng Homepage-->
 
-<div class="Homepage-Content">
-    <!--Unang Part-->
-      <section class="Overview">
-         <div class="left-side-container">
-            <h1>We advocate peaceful co-existence between the humans and furry friends...</h1>
-            <a href="cat-profile.php"><button>Cat Profiles</button></a>
-         </div>
+    <div class="Homepage-Content">
+        <!--Unang Part-->
+        <section class="Overview">
+            <div class="left-side-container">
+                <h1>We advocate peaceful co-existence between the humans and furry friends...</h1>
+                <a href="cat-profile.php"><button>Cat Profiles</button></a>
+            </div>
 
-         <div class="right-side-container">
-        <!--Photo Slider Section-->
-            <section class="container">
-                <div class="slider-wrapper">
-                    
-                    <div class="slider">
-                        <img id="slide-1" src="img/Slider1.png">
-                        <img id="slide-2" src="img/Slider2.png">
-                        <img id="slide-3" src="img/Slider3.png">
-                    
+            <div class="right-side-container">
+                <!--Photo Slider Section-->
+                <section class="container">
+                    <div class="slider-wrapper">
+
+                        <div class="slider">
+                            <img id="slide-1" src="img/Slider1.png">
+                            <img id="slide-2" src="img/Slider2.png">
+                            <img id="slide-3" src="img/Slider3.png">
+
+                        </div>
+                        <div class="slider-nav">
+                            <a href="#slide-1"></a>
+                            <a href="#slide-2"></a>
+                            <a href="#slide-3"></a>
+                        </div>
                     </div>
-                    <div class="slider-nav">
-                        <a href="#slide-1"></a>
-                        <a href="#slide-2"></a>
-                        <a href="#slide-3"></a>
-                    </div>
-                </div>
-            </section>
+                </section>
 
-         </div>
-      </section>
+            </div>
+        </section>
 
- <!--Pangalawang Part-->
-      <section class="Information">
-        <div class="left-side-container">
-            <img id="AduCatCommunity"src="img/AduCatCommunity.png">
+        <!--Pangalawang Part-->
+        <section class="Information">
+            <div class="left-side-container">
+                <img id="AduCatCommunity" src="img/AduCatCommunity.png">
+            </div>
+
+            <div class="right-side-container">
+                <h1>Why we need Cats in the Campus?</h1>
+                <p>Cats play a major role in improving both students’ health and their psychological well-being through decreasing anxiety. According to Dabbos, “Animals reduce anxiety and stress because they release a chemical that actually works on reducing the stress.”</p>
+
+            </div>
+
+        </section>
+
+        <!--Last Part-->
+
+        <section class="Links">
+            <div class="left-side-container">
+                <h2>Ready to welcome a University Cat into your life? Adopt now and make a feline friend!</h2>
+                <p>Experience the joy of welcoming a University Cat into your home. Begin your adoption journey now and give a cat a forever family!</p>
+                <a href="adoption.php"><button>Request for Adoption</button></a>
+            </div>
+
+            <div class="right-side-container">
+                <h2>Support our University Cats by ensuring they're well-fed and healthy!</h2>
+                <p>Your donation helps us provide essential care for our University Cats, ensuring they stay healthy and happy. Support our cause today and make a difference in the lives of these beloved feline companions!</p>
+                <a href="donation.php"><button>Donate</button></a>
+            </div>
+
+        </section>
+
+    </div>
+
+
+    <!--FOOTER NA TOH-->
+
+    <footer>
+        <div class="AduCatsLogo">
+            <img id="FooterLogo" src="img/Cat-1.png">
+            <p>© 2024 AduCats</p>
+        </div>
+        <div class="ContactUs">
+            <h3>CONTACT US</h3>
+            <a href="https://www.facebook.com/groups/946027585595117">Aducats Facebook</a>
+            <p>0992 123 4561</p>
+        </div>
+        <div class="Address">
+            <h3>ADDRESS</h3>
+            <p>900 San Marcelino St, Ermita, Manila, 1000 Metro Manila</p>
         </div>
 
-        <div class="right-side-container">
-            <h1>Why we need Cats in the Campus?</h1>
-            <p>Cats play a major role in improving both students’ health and their psychological well-being through decreasing anxiety. According to Dabbos, “Animals reduce anxiety and stress because they release a chemical that actually works on reducing the stress.”</p>
-
-        </div>
-
-      </section>
-
-<!--Last Part-->
-
-<section class="Links">
-    <div class="left-side-container">
-        <h2>Ready to welcome a University Cat into your life? Adopt now and make a feline friend!</h2>
-        <p>Experience the joy of welcoming a University Cat into your home. Begin your adoption journey now and give a cat a forever family!</p>
-        <a href="adoption.php"><button>Request for Adoption</button></a>
-    </div>
-
-    <div class="right-side-container">
-        <h2>Support our University Cats by ensuring they're well-fed and healthy!</h2>
-        <p>Your donation helps us provide essential care for our University Cats, ensuring they stay healthy and happy. Support our cause today and make a difference in the lives of these beloved feline companions!</p>
-        <a href="donation.php"><button>Donate</button></a>
-    </div>
-
-  </section>
-
-</div>
-
-
-<!--FOOTER NA TOH-->
-
-  <footer>
-     <div class="AduCatsLogo">
-        <img id="FooterLogo"src="img/Cat-1.png">
-        <p>© 2024 AduCats</p>
-    </div>
-    <div class="ContactUs">
-        <h3>CONTACT US</h3>
-        <a href="https://www.facebook.com/groups/946027585595117">Aducats Facebook</a>
-        <p>0992 123 4561</p>
-    </div>
-    <div class="Address">
-        <h3>ADDRESS</h3>
-        <p>900 San Marcelino St, Ermita, Manila, 1000 Metro Manila</p>
-    </div>
-     
-  </footer>
+    </footer>
 
 
 
 
 </body>
+
 </html>
